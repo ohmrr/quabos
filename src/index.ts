9f0806ae0f4b053ed69ec9b3c10f1fb263a843ff
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2023-04-28 22:44:26 -0700

    add Command interface and start command handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import {
+  Client,
+  Collection,
+  GatewayIntentBits,
+  SlashCommandBuilder,
+  REST,
+  Routes,
+} from 'discord.js';
 import path from 'path';
 import fs from 'fs';
 import 'dotenv/config';
@@ -13,6 +20,61 @@ const client = new Client({
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+interface Command {
+  data: SlashCommandBuilder;
+  cooldown?: number;
+  permissions?: string[];
+  execute: (interaction: any) => Promise<void>;
+}
+
+const commands = new Collection<string, Command>();
+
+const ping: Command = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong! SO COOL!'),
+  execute: async (interaction) => {
+    await interaction.reply('Pong!');
+  },
+};
+
+commands.set(ping.data.name, ping);
+
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.DEV_GUILD_ID),
+      { body: commands.map((command) => command.data.toJSON()) }
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    });
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
